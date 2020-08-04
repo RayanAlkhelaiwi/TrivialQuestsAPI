cf3780504773bf2bce8a2701b93f0e5e99eca903
RayanAlkhelaiwi <r.n.m.k@live.com>
2020-08-05 00:42:05 +0300

    Add unit tests for endpoints success/fail scenarios

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -18,6 +18,14 @@ class TriviaTestCase(unittest.TestCase):
         self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        # Inserting a question
+        self.new_question = {
+            "question": "What does the acronym SMTP represent?",
+            "answer": "Simple Mail Transport Protocol",
+            "category": "Technology",
+            "difficulty": 3
+        }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -30,10 +38,104 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     """
-    TODO
+    [Complete] TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_paginated_questions(self):
+        """ Test for getting successful paginated questions """
+
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_404_requesting_beyond_valid_pagination(self):
+        """ Test for 404 requesting beyond valid pagination """
+
+        res = self.client().get('/questions?page=1000', json={'difficulty': 3})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    def test_delete_question(self):
+        """ Test for deleting a question """
+
+        #! Increment question_id before execution
+        question_id = 1
+
+        res = self.client().delete('/questions/' + str(question_id))
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == question_id).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], question_id)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertEqual(question, None)
+
+    def test_404_deleting_nonexistent_question(self):
+        """ Test for 404 deleting a nonexistent question """
+
+        res = self.client().delete('/questions/300')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    def test_create_question(self):
+        """ Test for creating a question """
+
+        res = self.client().post(
+            '/questions', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created_question_id'])
+        self.assertTrue(data['total_questions'])
+
+    def test_405_create_nonexistent_question(self):
+        """ Test for 405 creating a nonexistent question """
+
+        res = self.client().post(
+            '/questions/300', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Method Not Allowed')
+
+    def test_search_question_with_results(self):
+        """ Test for searching a questions with results returned """
+
+        res = self.client().post('/questions', json={'search': 'What'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'] > 0)
+
+    def test_search_question_no_results(self):
+        """ Test for searching a question that returns no results """
+
+        res = self.client().post(
+            '/questions', json={'search': 'Prooggraammiing'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['questions'], 0)
+        self.assertEqual(data['total_questions'], 0)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
